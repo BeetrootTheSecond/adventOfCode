babd69d06d919c47e96f33a1cf3759957541635d
Beetroot <aaronhorrell@hotmail.co.uk>
2021-12-04 22:56:55 +0000

    Day 2 of 2021 Two Star

--- 2021/2/index.ts
+++ 2021/2/index.ts
@@ -15,16 +15,16 @@ let depth: number = 0;
 
 for (let i = 0; i < data.length; i++) {
   //console.log([data[i][0], data[i]]);
-  switch (data[i][0][0]) {
-    case "f": {
+  switch (data[i][0]) {
+    case "forward": {
       horizontal += parseInt(data[i][1]);
       break;
     }
-    case "d": {
+    case "down": {
       depth += parseInt(data[i][1]);
       break;
     }
-    case "u": {
+    case "up": {
       depth -= parseInt(data[i][1]);
       break;
     }
@@ -32,4 +32,33 @@ for (let i = 0; i < data.length; i++) {
 }
 
 console.log([depth, horizontal]);
-console.log("One Star", depth * horizontal);
+console.log(["One Star", depth * horizontal]);
+
+let horizontalTwoStar: number = 0;
+let depthTwoStar: number = 0;
+let aimTwoStar: number = 0;
+
+for (let i = 0; i < data.length; i++) {
+  //console.log([horizontalTwoStar, depthTwoStar, aimTwoStar]);
+  //console.log([data[i][0], data[i]]);
+
+  let value: number = parseInt(data[i][1]);
+  switch (data[i][0]) {
+    case "forward": {
+      horizontalTwoStar += value;
+      depthTwoStar += aimTwoStar * value;
+      break;
+    }
+    case "down": {
+      aimTwoStar += value;
+      break;
+    }
+    case "up": {
+      aimTwoStar -= value;
+      break;
+    }
+  }
+}
+
+console.log([depthTwoStar, horizontalTwoStar]);
+console.log(["Two Star", depthTwoStar * horizontalTwoStar]);
